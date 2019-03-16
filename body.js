edf2c6dc3524781c098a6c14f40624d39850b27b
Sesamestrong <doolie.healey@gmail.com>
2019-03-15 20:12:41 -0400

    Initial commit.

---
+++ body.js
@@ -0,0 +1,10 @@
+import BodyPart from "body-part";
+
+/**
+ * Describes the Body class, which interacts with bodies near it and coordinates the magical orchestra that is movement.
+ * @constructor
+ */
+function Body(){
+
+}
+export default Body;
---
+++ index.js
@@ -0,0 +1,2 @@
+import * as THREE from "three";
+//Threejs stuff
