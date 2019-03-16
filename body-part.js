59c48ea17f3928b390e5d9be3d98976d3e5c57cc
Sesamestrong <doolie.healey@gmail.com>
2019-03-15 20:20:43 -0400

    Added body-part.js

---
+++ body-part.js
@@ -0,0 +1,14 @@
+/**
+ * Describes the BodyPart class, which holds a Body together and is in charge of determining structure of a body to render. It can send events such as a damage-taking event to the main Body, as well as receive and pass down hits.
+ * @constructor
+ * @param {Number} size - The size of the BodyPart - Determines what body parts can follow others.
+ */
+function BodyPart(size=4){
+  let ret={};
+  
+  function init(){
+      
+  }
+
+}
+export default BodyPart;
