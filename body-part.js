a8782f7657ad42a824041b40d82b3a8489effebe
Sesamestrong <doolie.healey@gmail.com>
2019-03-15 20:25:49 -0400

    Added init

--- body-part.js
+++ body-part.js
@@ -1,11 +1,16 @@
 /**
  * Describes the BodyPart class, which holds a Body together and is in charge of determining structure of a body to render. It can send events such as a damage-taking event to the main Body, as well as receive and pass down hits.
  * @constructor
- * @param {Number} size - The size of the BodyPart - Determines what body parts can follow others.
+ * @param size {Number} - The size of the BodyPart - Determines what body parts can follow others.
  */
-function BodyPart(size=4){
+function BodyPart(size=4,type="body"){
   let ret={};
   
+  /**
+   * Initializes the body part
+   * @param parent {BodyPart} - The parent BodyPart that will control this BodyPart.
+   * @param children {BodyPart[]} - The children BodyParts that this BodyPart will control - of a specified BodyPart type.
+   */
   function init(){
       
   }
